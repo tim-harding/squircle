62e187d0f6d628d7afc9b1a2de182cc72b175a48
Tim Harding <Tim@TimHarding.co>
2024-09-16 15:09:03 -0700

    Path improvements for NaN points

--- worklet.mjs
+++ worklet.mjs
@@ -8,46 +8,7 @@
  * @property {number} height
  */
 
-/*
-float n = chf("n");
-n = 1/f@r;
-
-float t = @P.y;
-float ct = cos(t);
-float st = sin(t);
-float i = 2/n;
-
-float x = pow(ct, i);
-float y = pow(st, i);
-@P = set(x,y,0);
-
-float dx = -i * st * pow(ct, i-1);
-float dy = i * ct * pow(st, i-1);
-@N = set(dx, dy, 0);
-*/
-
-/**
- * @param {number} r Radius
- * @param {number} s Segments
- */
-function superellipse_parametric(r, s) {
-  const r2 = 2 * r;
-  const r21 = r2 - 1;
-  const n = 1 / r;
-  const out = new Array(4);
-  for (let i = 0; i < s + 1; i++) {
-    const t = i / s;
-    const ct = Math.cos(t);
-    const st = Math.sin(t);
-    const x = ct ** r2;
-    const y = st ** r2;
-    const dx = -r2 * st * ct ** r21;
-    const dy = r2 * ct * st ** r21;
-  }
-}
-
-const SEGMENTS = 4;
-const CACHE = new Float32Array((SEGMENTS + 1) * 2);
+const SEGMENTS = 3;
 
 class Squircle {
   static get contextOptions() {
@@ -67,41 +28,47 @@ class Squircle {
     const { width: w, height: h } = size;
     /** @type {CSSUnitValue} */
     const radius = props.get("--radius");
-    // const r = Math.min(radius.value, w / 2, h / 2);
-
-    const r = 32;
-
+    const l = Math.min(w, h) / 2;
+    const r = Math.min(radius.value, l);
+    const n = r / l;
     const den = Math.PI / 2 / SEGMENTS;
+
+    ctx.moveTo(w, h - l);
+    for (let i = 1; i < SEGMENTS; i++) {
+      const t = i * den;
+      const x = Math.cos(t) ** n * l + w - l;
+      const y = Math.sin(t) ** n * l + h - l;
+      ctx.lineTo(x, y);
+    }
+    ctx.lineTo(w - l, h);
+
+    ctx.lineTo(l, h);
+    for (let i = SEGMENTS + 1; i < SEGMENTS * 2; i++) {
+      const t = i * den;
+      const x = -((-Math.cos(t)) ** n) * l + l;
+      const y = Math.sin(t) ** n * l + h - l;
+      ctx.lineTo(x, y);
+    }
+    ctx.lineTo(0, h - l);
+
+    ctx.lineTo(0, l);
+    for (let i = SEGMENTS * 2 + 1; i < SEGMENTS * 3; i++) {
+      const t = i * den;
+      const x = -((-Math.cos(t)) ** n) * l + l;
+      const y = -((-Math.sin(t)) ** n) * l + l;
+      ctx.lineTo(x, y);
+    }
+    ctx.lineTo(l, 0);
+
+    ctx.lineTo(w - l, 0);
+    for (let i = SEGMENTS * 3 + 1; i < SEGMENTS * 4; i++) {
+      const t = i * den;
+      const x = Math.cos(t) ** n * l + w - l;
+      const y = -((-Math.sin(t)) ** n) * l + l;
+      ctx.lineTo(x, y);
+    }
+
     ctx.fillStyle = "white";
-    ctx.moveTo(w, h - r);
-    for (let i = 1; i < SEGMENTS + 1; i++) {
-      const t = i * den;
-      const x = Math.cos(t) ** 1 * r + w - r;
-      const y = Math.sin(t) ** 1 * r + h - r;
-      ctx.lineTo(x, y);
-    }
-
-    for (let i = SEGMENTS; i < SEGMENTS * 2 + 1; i++) {
-      const t = i * den;
-      const x = Math.cos(t) ** 1 * r + r;
-      const y = Math.sin(t) ** 1 * r + h - r;
-      ctx.lineTo(x, y);
-    }
-
-    for (let i = SEGMENTS * 2; i < SEGMENTS * 3 + 1; i++) {
-      const t = i * den;
-      const x = Math.cos(t) ** 1 * r + r;
-      const y = Math.sin(t) ** 1 * r + r;
-      ctx.lineTo(x, y);
-    }
-
-    for (let i = SEGMENTS * 3; i < SEGMENTS * 4 + 1; i++) {
-      const t = i * den;
-      const x = Math.cos(t) ** 1 * r + w - r;
-      const y = Math.sin(t) ** 1 * r + r;
-      ctx.lineTo(x, y);
-    }
-
     ctx.closePath();
     ctx.fill();
   }
