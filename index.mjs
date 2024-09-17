44fc92e08b3fb4fb85fad7e09b9d016c6e0f2f3d
Tim Harding <Tim@TimHarding.co>
2024-09-16 18:57:48 -0700

    Set squircle border in CSS

--- index.mjs
+++ index.mjs
@@ -2,10 +2,31 @@ export function register() {
   if (!CSS || !CSS.registerProperty || !CSS.paintWorklet) return;
 
   CSS.registerProperty({
-    name: "--radius",
+    name: "--squircle-radius",
     syntax: "<length>",
     inherits: false,
     initialValue: "0px",
+  });
+
+  CSS.registerProperty({
+    name: "--squircle-border-width",
+    syntax: "<length>",
+    inherits: false,
+    initialValue: "0px",
+  });
+
+  CSS.registerProperty({
+    name: "--squircle-fill",
+    syntax: "<color>",
+    inherits: true,
+    initialValue: "transparent",
+  });
+
+  CSS.registerProperty({
+    name: "--squircle-border-color",
+    syntax: "<color>",
+    inherits: true,
+    initialValue: "transparent",
   });
 
   CSS.paintWorklet.addModule("/worklet.mjs");
--- worklet.mjs
+++ worklet.mjs
@@ -8,13 +8,23 @@
  * @property {number} height
  */
 
+const fills = [
+  ["", 0],
+  ["", 0],
+];
+
 class Squircle {
   static get contextOptions() {
     return { alpha: true };
   }
 
   static get inputProperties() {
-    return ["--radius"];
+    return [
+      "--squircle-radius",
+      "--squircle-fill",
+      "--squircle-border-width",
+      "--squircle-border-color",
+    ];
   }
 
   /**
@@ -24,31 +34,33 @@ class Squircle {
    */
   paint(ctx, size, props) {
     const { width, height } = size;
-    const radius = props.get("--radius").value;
+    const radius = props.get("--squircle-radius").value;
+    const fill = props.get("--squircle-fill");
+    const borderWidth = props.get("--squircle-border-width").value;
+    const borderColor = props.get("--squircle-border-color");
 
-    const fills = [
-      ["blue", 0],
-      ["red", 8],
-    ];
+    console.log(fill);
 
-    for (let fill = 0; fill < 2; fill++) {
-      const [fillColor, borderWidth] = fills[fill];
-      const w = Math.max(0, width - borderWidth * 2);
-      const h = Math.max(0, height - borderWidth * 2);
+    const drawBorder = borderWidth !== "0" && borderColor !== "transparent";
+    const drawFill = fill !== "transparent";
+
+    function paint(borderOffset) {
+      const w = Math.max(0, width - borderOffset * 2);
+      const h = Math.max(0, height - borderOffset * 2);
       const l = Math.min(w, h) / 2;
-      const r = Math.max(0, Math.min(radius - borderWidth * Math.SQRT1_2, l));
+      const r = Math.max(0, Math.min(radius - borderOffset * Math.SQRT1_2, l));
       const segments = Math.ceil(Math.sqrt(r)) * 4;
       const n = r / l;
       const den = Math.PI / 2 / segments;
 
       ctx.beginPath();
       ctx.resetTransform();
-      ctx.moveTo(w + borderWidth, h - l + borderWidth);
+      ctx.moveTo(w + borderOffset, h - l + borderOffset);
       for (let i = 0; i < 4; i++) {
         const left = i > 0 && i < 3;
         const top = i > 1;
-        const offset_x = (left ? l : w - l) + borderWidth;
-        const offset_y = (top ? l : h - l) + borderWidth;
+        const offset_x = (left ? l : w - l) + borderOffset;
+        const offset_y = (top ? l : h - l) + borderOffset;
         const o = i % 2;
         const e = 1 - o;
         const rotate_sign_y = left ? -1 : 1;
@@ -57,6 +69,7 @@ class Squircle {
         const m21 = o * rotate_sign_x * l;
         const m12 = o * rotate_sign_y * l;
         const m22 = e * rotate_sign_y * l;
+
         ctx.setTransform(m11, m21, m12, m22, offset_x, offset_y);
         for (let j = 0; j < segments + 1; j++) {
           const t = j * den;
@@ -66,9 +79,18 @@ class Squircle {
         }
       }
 
-      ctx.fillStyle = fillColor;
       ctx.closePath();
       ctx.fill();
+    }
+
+    if (drawBorder) {
+      ctx.fillStyle = borderColor;
+      paint(0);
+    }
+
+    if (drawFill) {
+      ctx.fillStyle = fill;
+      paint(borderWidth);
     }
   }
 }
