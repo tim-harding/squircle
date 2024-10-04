872d21b127712ac3dbead1a997b2625207873924
Tim Harding <Tim@TimHarding.co>
2024-10-04 12:51:07 -0700

    Add .min to built worklet

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
+rm package/*
 mkdir -p package
 cp src/index.css package/
 cp src/index.mjs package/
-node_modules/uglify-js/bin/uglifyjs --compress --mangle -- src/worklet.mjs > package/worklet.js
+node_modules/uglify-js/bin/uglifyjs --compress --mangle -- src/worklet.mjs > package/worklet.min.js
 sed 's/"private": true/"private": false/g' package.json > package/package.json
 
