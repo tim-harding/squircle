8f03cc288c6ef03b24e2609d005cd99d006c331a
Tim Harding <Tim@TimHarding.co>
2024-10-04 21:54:25 -0700

    Minify index.mjs in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@ mkdir -p package
 rm package/*
 cp README.md package/
 cp src/index.css package/
-cp src/index.mjs package/
-node_modules/uglify-js/bin/uglifyjs --compress --mangle -- src/worklet.mjs > package/worklet.min.js
 sed 's/"private": true/"private": false/g' package.json > package/package.json
+node_modules/uglify-js/bin/uglifyjs --compress --mangle -- src/worklet.mjs > package/worklet.min.js
+node_modules/uglify-js/bin/uglifyjs --compress --mangle -- src/index.mjs > package/index.min.js
+
 cp tsconfig.build.json package/tsconfig.json
 cd package
 ../node_modules/typescript/bin/tsc --build
+rm tsconfig.json tsconfig.tsbuildinfo
 cd ..
-rm package/tsconfig.json
-rm package/tsconfig.tsbuildinfo
