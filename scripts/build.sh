49e0e09c535b20372cd95db477a31787027cfdc1
Tim Harding <Tim@TimHarding.co>
2024-10-05 15:16:10 -0700

    Don't mangle props when minifying

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ cp README.md build/
 cp src/* build/
 sed 's/"private": true/"private": false/g' package.json > build/package.json
 npx tsc --project tsconfig.build.json
-npx rollup src/worklet.mjs | npx uglifyjs --compress --mangle --mangle-props > build/worklet.min.js
+npx rollup src/worklet.mjs | npx uglifyjs --compress --mangle > build/worklet.min.js
