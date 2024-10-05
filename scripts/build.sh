45ed91d050d2fd9c3fdef321519add106567937e
Tim Harding <Tim@TimHarding.co>
2024-10-05 14:59:40 -0700

    Add worklet bundling to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,7 @@
-mkdir -p package
-rm package/*
-cp README.md package/
-cp src/index.css package/
-sed 's/"private": true/"private": false/g' package.json > package/package.json
-node_modules/uglify-js/bin/uglifyjs --compress --mangle -- src/worklet.mjs > package/worklet.min.js
-node_modules/uglify-js/bin/uglifyjs --compress --mangle -- src/index.mjs > package/index.min.js
-
-cp tsconfig.build.json package/tsconfig.json
-cd package
-../node_modules/typescript/bin/tsc --build
-rm tsconfig.json tsconfig.tsbuildinfo
-cd ..
+mkdir -p build
+rm -f build/*
+cp README.md build/
+cp src/* build/
+sed 's/"private": true/"private": false/g' package.json > build/package.json
+npx tsc --project tsconfig.build.json
+npx rollup src/worklet.mjs | npx uglifyjs --compress --mangle --mangle-props > build/worklet.min.js
