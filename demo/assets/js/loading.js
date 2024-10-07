ec0016c11ad9612b1a5769d2cf218d41f5aa6bab
Tim Harding <Tim@TimHarding.co>
2024-10-07 13:55:01 -0700

    Fix demo bugs
    Updating APIs usage for 0.1.8

--- demo/assets/js/loading.js
+++ demo/assets/js/loading.js
@@ -1,15 +1,8 @@
-const IS_PAINT_SUPPORTED = CSS.supports("background", "paint(id)");
-
 const { promise, resolve } = Promise.withResolvers();
 export const loaded = promise;
 
 export async function load() {
-  await Promise.all([
-    loadSquircleComponent(),
-    loadPaintWorklet(),
-    loadComponents(),
-  ]);
-  console.log("loaded");
+  await Promise.all([loadPaintWorklet(), loadComponents()]);
   resolve(undefined);
 }
 
@@ -21,28 +14,16 @@ async function loadComponents() {
     import("./corner.js"),
   ].map((promise) =>
     promise.then(({ NAME, Component }) => {
-      console.log(NAME);
       customElements.define(NAME, Component);
     }),
   );
   await Promise.all(promises);
-  console.log("loaded components");
 }
 
 async function loadPaintWorklet() {
-  if (!IS_PAINT_SUPPORTED) return;
-  const { register } = await import(
+  const { register, createCustomElement } = await import(
     "https://unpkg.com/superellipse-squircle@0.1.8/index.mjs"
   );
   register("https://unpkg.com/superellipse-squircle@0.1.8/worklet.min.js");
-  console.log("Loaded worklet");
-}
-
-async function loadSquircleComponent() {
-  const promise = IS_PAINT_SUPPORTED
-    ? import("./squircle-houdini.js")
-    : import("./squircle-canvas.js");
-  const module = await promise;
-  customElements.define("th-squircle", module.default);
-  console.log("Loaded squircle");
+  createCustomElement("th-squircle");
 }
