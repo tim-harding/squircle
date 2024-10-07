e32bbb926ae8d949af8401c28c426a87f4f4ec20
Tim Harding <Tim@TimHarding.co>
2024-10-07 13:59:20 -0700

    Custom element needs to clear if not resized

--- src/squircle-canvas.js
+++ src/squircle-canvas.js
@@ -55,7 +55,7 @@ export default class SquircleCanvas extends HTMLElement {
         canvas.width = Math.round(this._width);
         canvas.height = Math.round(this._height);
         ctx.scale(devicePixelRatio, devicePixelRatio);
-        this._draw();
+        this._draw(false);
       }
     });
     observer.observe(this);
@@ -98,19 +98,22 @@ export default class SquircleCanvas extends HTMLElement {
 
   _scheduleRedraw() {
     if (this._animationFrame >= 0) return;
-
-    const context = this._context;
-    if (context === null) return;
-
     this._animationFrame = requestAnimationFrame(() => {
       this._animationFrame = -1;
-      this._draw();
+      this._draw(true);
     });
   }
 
-  _draw() {
+  /**
+   * @param {boolean} isDirty Whether the canvas needs to be cleared first
+   */
+  _draw(isDirty) {
     const { _context: ctx } = this;
     if (ctx === null) return;
+
+    if (isDirty) {
+      ctx.clearRect(0, 0, this._width, this._height);
+    }
 
     paint(
       ctx,
