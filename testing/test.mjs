7e3f44945541e1e9db050a9976a718cd76b3faf7
Tim Harding <Tim@TimHarding.co>
2024-10-08 12:13:11 -0700

    Combine testing files into index.html

--- testing/test.mjs
+++
@@ -1,8 +0,0 @@
-import { register, path, createCustomElement } from "/src/index.mjs";
-
-register("/src/worklet.mjs");
-createCustomElement("my-squircle");
-
-const svg = document.getElementById("squircle-svg");
-const d = path(0, 0, 512, 256, 32);
-svg.setAttribute("d", d);
